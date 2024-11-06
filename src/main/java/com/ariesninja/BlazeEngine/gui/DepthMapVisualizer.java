a6aacaa0d68b7dfb8c2b17581227202a374f3836
Aries Powvalla <aries@ninjam.co>
2024-11-06 13:35:51 -0800

    Revert "Test 2"
    This reverts commit e826feab92496a1843dbe939c8fe16255394bce9.

--- src/main/java/com/ariesninja/BlazeEngine/gui/DepthMapVisualizer.java
+++ src/main/java/com/ariesninja/BlazeEngine/gui/DepthMapVisualizer.java
@@ -6,22 +6,12 @@ import java.awt.image.BufferedImage;
 
 public class DepthMapVisualizer extends JFrame {
     private BufferedImage depthMapImage;
-    private JPanel panel;
 
     public DepthMapVisualizer(int width, int height) {
         setTitle("Depth Map Visualizer");
         setSize(width, height);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         depthMapImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-
-        panel = new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.drawImage(depthMapImage, 0, 0, null);
-            }
-        };
-        add(panel);
     }
 
     public void updateDepthMap(double[][] depthMap) {
@@ -32,6 +22,12 @@ public class DepthMapVisualizer extends JFrame {
                 depthMapImage.setRGB(x, y, rgb);
             }
         }
-        panel.repaint();
+        repaint();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.drawImage(depthMapImage, 0, 0, null);
     }
 }
